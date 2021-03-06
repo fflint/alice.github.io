d4bf90cf8fb4517923943f26ea163e577d28f0ae
unknown <3013698623@qq.com>
2021-03-06 11:13:21 +0800

    Site updated: 2021-03-06 11:13:21

--- js/alice.js
+++ js/alice.js
@@ -14,7 +14,7 @@ document.addEventListener('visibilitychange', function() {
     }
 });
 //隐藏首页图
-// var full_page = document.getElementsByClassName("full_page");
-// if (full_page.length != 0) {
-//     full_page[0].style.background = "transparent";
-// }
+var full_page = document.getElementsByClassName("full_page");
+if (full_page.length != 0) {
+    full_page[0].style.background = "transparent";
+}
